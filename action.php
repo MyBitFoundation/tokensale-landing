9aa5bc0bac72ad580ab969be530c26f2e93e8bb7
Sergey Furs <itscorpeon@gmail.com>
2017-04-29 17:51:54 +0300

    rm old module

--- action.php
+++ action.php
@@ -2,9 +2,7 @@
 
 session_start();
 
-require_once('classes/lib/db.php');
 require_once('classes/common.php');
-require_once('classes/model/user.php');
 
 if (!isset($_REQUEST['action'])){
     header('Location: /');
@@ -16,74 +14,6 @@ $errors     = array();
 $data       = $_REQUEST;
 
 switch ($_REQUEST['action']) {
-
-    case 'registration':
-        if(!isset($data['email']) || !$data['email'] || !filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-            $result = false;
-            $errors['registerEmail'] = 'Incorrect e-mail';
-        } else {
-            if(User::getInstance()->checkExistUserByEmail($data['email'])) {
-                $result = false;
-                $errors['registerEmail'] = 'This e-mail already exists';
-            }
-        }
-
-        if(!isset($data['password']) || !$data['password'] || mb_strlen($data['password']) < 5) {
-            $result = false;
-            $errors['registerPassword'] = 'The password must be at least 5 characters';
-        }
-
-        if(!isset($data['passwordCopy']) || !$data['passwordCopy'] || $data['password'] != $data['passwordCopy']) {
-            $result = false;
-            $errors['registerPasswordCopy'] = 'Passwords do not match';
-        }
-
-        if($result) {
-            $user_id = User::getInstance()->addUser($data['email'],$data['password']);
-
-            $hash = User::getInstance()->login($data['email'],$data['password']);
-
-            if($hash) {
-                setcookie('user_hash', $hash, time() + 60 * 60 * 24 * 30, '/');
-            }
-        }
-
-        $response = array(
-            'result' => $result,
-            'errors' => $errors,
-        );
-
-        break;
-
-    case 'authorisation':
-        if(!isset($data['email']) || !$data['email'] || !filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-            $result = false;
-            $errors['loginEmail'] = 'Incorrect e-mail';
-        }
-
-        if(!isset($data['password']) || !$data['password'] || mb_strlen($data['password']) < 5) {
-            $result = false;
-            $errors['loginPassword'] = 'The password must be at least 5 characters';
-        }
-
-        if($result) {
-            $hash = User::getInstance()->login($data['email'],$data['password']);
-
-            if($hash) {
-                setcookie('user_hash', $hash, time()+60*60*24*30, '/');
-            } else {
-                $result = false;
-                $errors['loginEmail'] = 'E-mail or Password incorrect';
-            }
-        }
-
-        $response = array(
-            'result' => $result,
-            'errors' => $errors,
-        );
-
-
-        break;
 
     case 'subscribe':
 
